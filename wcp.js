bf99823b211fc4eedd8cdc92dc3e7906f457858d
webcodeprotector <webcodeprotector@gmail.com>
2026-04-12 16:51:33 +0500

    Create wcp.js

---
+++ wcp.js
@@ -0,0 +1,5 @@
+const users = [
+    { username: 'guest', pwd: '1234', expiry: new Date('2099-12-31') },
+    { username: 'user1', pwd: 'pass1', expiry: new Date('2024-10-01') },
+    { username: 'user2', pwd: 'pass2', expiry: new Date('2023-01-01') }
+];
